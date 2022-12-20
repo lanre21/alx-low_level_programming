6655064538de42af27efcdc9f072334c01b9764c
lanre21 <oluwatunbihezekiah@gmail.com>
2022-12-20 14:55:44 -0800

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,496 @@
+#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}#incude "kasiye.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer to swap
+  * @b: integer to swap
+  */
+
+void sawp_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}
